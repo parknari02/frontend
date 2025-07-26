3f1a3e1a50bfeca2bf0ed08a32d88adeabe455fb
parknari02 <pnr021118@naver.com>
2025-07-26 23:07:37 +0900

    style: 검색필드 스타일 변경

--- src/components/common/SearchBar.jsx
+++ src/components/common/SearchBar.jsx
@@ -3,29 +3,37 @@ import searchIcon from '../../assets/icons/search.svg';
 
 const SearchBar = () => {
   return (
-    <SearchInput>
-      <SearchText>✷ Search</SearchText>
+    <SearchInputWrapper>
+      <SearchField type="text" placeholder="✷ Search" />
       <SearchIcon src={searchIcon} alt="search" />
-    </SearchInput>
+    </SearchInputWrapper>
   );
 };
 
 export default SearchBar;
 
-const SearchInput = styled.div`
+const SearchInputWrapper = styled.label`
   background-color: ${({ theme }) => theme.mainLight};
   border-radius: 40px;
   padding: 13px 20px;
   display: flex;
   align-items: center;
+  height: 40px;
+  gap: 8px;
+`;
+
+const SearchField = styled.input`
+  background: transparent;
+  border: none;
+  outline: none;
+  flex: 1;
   color: white;
   font-size: 12px;
   font-weight: 700;
-  height: 40px;
-`;
 
-const SearchText = styled.span`
-  flex: 1;
+  &::placeholder {
+    color: white;
+  }
 `;
 
 const SearchIcon = styled.img`
