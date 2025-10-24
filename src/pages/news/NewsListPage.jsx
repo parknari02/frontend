e2fd945a823990de793963af2f8142d313d91040
parknari02 <pnr021118@naver.com>
2025-10-24 20:05:46 +0900

    feat: 검색 연결

--- src/pages/news/NewsListPage.jsx
+++ src/pages/news/NewsListPage.jsx
@@ -1,15 +1,26 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 import Header from '../../components/common/Header';
 import SearchBar from '../../components/common/SearchBar';
 import NewsDetailList from '../../components/news/list/NewsDetailList';
 
 const NewsListPage = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearch = (query) => {
+        setSearchQuery(query);
+    };
+
     return (
         <PageContainer>
             <Header content='news' />
             <ContentContainer>
-                <SearchBar bgColor="transparent" />
-                <NewsDetailList />
+                <SearchBar
+                    bgColor="transparent"
+                    onSearch={handleSearch}
+                    initialValue={searchQuery}
+                />
+                <NewsDetailList searchQuery={searchQuery} />
             </ContentContainer>
         </PageContainer>
     );
